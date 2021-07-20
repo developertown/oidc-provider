a1bb9576307f3230e163dd46bdf51c03004a57dc
Alex <abillingsley@developertown.com>
2021-07-20 10:03:07 -0400

    expose token type in public interface

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Auth0Provider, useAuth0 } from "./auth0-provider";
 export { AzureProvider, useAzure } from "./azure-provider";
 export { CognitoProvider, useCongito } from "./cognito-provider";
 export { OIDCProvider, useAuth } from "./oidc-provider";
+export type { Token } from "./oidc-provider";
 export { StorageTypes } from "./token-storage";
 export type { TokenStorage };
 export { withAuthenticationRequired };
